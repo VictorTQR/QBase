9d2391a5c32297503aa0e5dcf71db94d54a1f7d7
Victor <18890258271@163.com>
2026-02-26 20:28:36 +0800

    feat: add ui store for panel state management

---
+++ app/src/stores/ui.js
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useUiStore = defineStore('ui', () => {
+  const activeModule = ref('chat')
+  const isAgentPanelVisible = ref(true)
+  const isFlashcardFocusMode = ref(false)
+
+  function setActiveModule(moduleId) {
+    activeModule.value = moduleId
+  }
+
+  function toggleAgentPanel() {
+    isAgentPanelVisible.value = !isAgentPanelVisible.value
+  }
+
+  function toggleFlashcardFocusMode() {
+    isFlashcardFocusMode.value = !isFlashcardFocusMode.value
+  }
+
+  return {
+    activeModule,
+    isAgentPanelVisible,
+    isFlashcardFocusMode,
+    setActiveModule,
+    toggleAgentPanel,
+    toggleFlashcardFocusMode
+  }
+})
