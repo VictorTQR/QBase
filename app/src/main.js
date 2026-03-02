edd5c16d0c1cb24a4ef50c0b3e0db27666990525
Victor <18890258271@163.com>
2026-03-02 18:35:37 +0800

    refactor: 在 main.js 引入全局动画

--- app/src/main.js
+++ app/src/main.js
@@ -8,6 +8,8 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import './styles/animations.css'
+
 const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
