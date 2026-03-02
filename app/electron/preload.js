25a5cae854e8bfddf9ee66109f3624147f622160
Victor <18890258271@163.com>
2026-03-02 22:01:16 +0800

    refactor: 清理 electron 中的 mineru 遗留 IPC 处理器

--- app/electron/preload.js
+++ app/electron/preload.js
@@ -9,15 +9,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
   readDir: (dirPath) => ipcRenderer.invoke('read-dir', dirPath),
   getFileStats: (filePath) => ipcRenderer.invoke('get-file-stats', filePath),
   searchFiles: (folderPath, query) => ipcRenderer.invoke('search-files', folderPath, query),
-  mineru: {
-    createUploadUrls: (files, apiKey) => ipcRenderer.invoke('mineru:create-upload-urls', files, apiKey),
-    uploadFile: (url, fileData) => ipcRenderer.invoke('mineru:upload-file', url, fileData),
-    submitTask: (batchId, apiKey) => ipcRenderer.invoke('mineru:submit-task', batchId, apiKey),
-    pollTaskStatus: (taskId, apiKey) => ipcRenderer.invoke('mineru:poll-task-status', taskId, apiKey),
-    downloadResult: (url) => ipcRenderer.invoke('mineru:download-result', url),
-    extractPdf: (filePath, config) => ipcRenderer.invoke('mineru:extract-pdf', filePath, config),
-    testConnection: (config) => ipcRenderer.invoke('mineru:test-connection', config)
-  },
   siliconflow: {
     createEmbedding: (text, config) => ipcRenderer.invoke('siliconflow:create-embedding', text, config)
   }
